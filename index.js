db12d92c1f503c696bea83ea2828998722bdeaa5
MaksumulEmon <maksumulemon300@gmail.com>
2026-07-27 20:03:51 +0600

    All project feature done & deploy

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    origin: [`${process.env.CLIENT_URL}`],
+    // , 'http://127.0.0.1:3000'
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'x-session-token']
@@ -31,7 +32,7 @@ let db;
 
 async function connectDB() {
     try {
-        await client.connect();
+        // await client.connect();
         db = client.db("playzone");
         console.log("Connected successfully to MongoDB database: playzone");
         await seedInitialPlaygrounds();
